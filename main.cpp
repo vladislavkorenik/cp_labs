f017dd568476781f6532071a2587f4e0a47162b2
Evgeny Koleda <evgeny.koleda@katemedia.eu>
2020-09-10 23:13:45 +0300

    Complete first lab

--- main.cpp
+++ main.cpp
@@ -4,66 +4,70 @@
 
 using namespace std;
 
-string generateMessage(const string& format,const string& message, const string& prior){
-    string str = format;
-    string tags[] = {"*prior*", "%data%", "%%time%%", "**message**"};
+class Logger
+{
+public:
+    string format = "-prior- | %data% %%time%% --message--";
+    void setFormat();
+    void printError(const string &text);
+    void printDebug(const string &text);
+    void printTrace(const string &text);
+    string generateMessage(const string &message, const string &prior);
+};
 
-    for (int i = 0; i < tags->length(); ++i) {
+void Logger::printError(const string &text)
+{
+    string result = this->generateMessage(text, "Error");
+
+    cout << "Formated: " << result << endl;
+}
+
+void Logger::printDebug(const string &text)
+{
+    cout << text;
+}
+
+void Logger::printTrace(const string &text)
+{
+    cout << text;
+}
+
+void Logger::setFormat()
+{
+    cout << "Input format:";
+    getline(cin, this->format);
+
+    cout << "Your current format is: " << this->format << endl;
+}
+
+string Logger::generateMessage(const string &message, const string &prior)
+{
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+
+    string str = this->format;
+
+    string tags[] = {"-prior-", "%data%", "%%time%%", "--message--"};
+    string data = to_string(ltm->tm_mday) + "." + to_string(1 + ltm->tm_mon) + "." + to_string(1900 + ltm->tm_year);
+    string time = to_string(ltm->tm_hour) + ":" + to_string(ltm->tm_min) + ":" + to_string(ltm->tm_sec);
+    string replacements[] = {prior, data, time, message};
+
+    for (int i = 0; i < sizeof(tags) / sizeof(*tags); i++)
+    {
         regex rx(tags[i]);
-        string replacement;
 
-        time_t now = time(0);
-        tm *ltm = localtime(&now);
-
-        switch (i) {
-            case 0: replacement = prior; break;
-            case 1: replacement = to_string(ltm->tm_mday) + "." +  to_string(1 + ltm->tm_mon) + "." + to_string(1900 + ltm->tm_year); break;
-            case 2: replacement = to_string(ltm->tm_hour) + ":" + to_string(ltm->tm_min) + ":" + to_string(ltm->tm_sec); break;
-            case 3: replacement = message;
-        }
-
-        str = regex_replace(str, rx, replacement);
+        str = regex_replace(str, rx, replacements[i]);
     }
 
     return str;
 }
 
-class Logger {
-public:
-    string format = "*prior* | %data% %%time%% **message**";
-    void setFormat();
-    void printError(const string& text);
-    void printDebug(const string& text);
-    void printTrace(const string& text);
-};
-
-void Logger::printError(const string& text) {
-
-    regex rx("lol");
-    string replacement = "whoa";
-    cout << regex_replace(text, rx, replacement) << endl;
-}
-
-void Logger::printDebug(const string& text){
-    cout << text;
-}
-
-void Logger::printTrace(const string& text){
-    cout << text;
-}
-
-void Logger::setFormat() {
-    cout << "Input format:";
-    cin >> this->format;
-
-    cout << "Your current format is: " << this->format << endl;
-}
-
-int main() {
+int main()
+{
     Logger logger;
     logger.printError("lol it,s Error");
     logger.setFormat();
+    logger.printError("lol it,s Error");
 
-    system("pause");
     return 0;
 }
